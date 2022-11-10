d065ab25a6e890d11f4bd5af0fe76fecc7b61d8c
mide <efusanyaae@gmail.com>
2022-11-10 22:58:07 +0100

    update

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,2 +1,2 @@
-export const supportedSpendContexts = ['valora', 'evm', 'mobile', 'web', 'chimoney_redeem','status', 'metamask','walletconnect','celo'];
+export const supportedSpendContexts = ['valora', 'evm', 'mobile', 'web', 'chimoney_redeem','status', 'metamask','walletconnect','celo', "api_call"];
 export const supportedAppStyles = ['light', 'dark', 'moonlight', 'royal'];
