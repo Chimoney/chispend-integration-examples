bb59c323e4d521d927ca78ea29098ed95c608c4a
mide <efusanyaae@gmail.com>
2022-11-10 21:39:25 +0100

    update: support mobile csContext and add pre to copy transaction object

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,61 +1,65 @@
-import { useMediaQuery } from 'react-responsive';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import _ from 'lodash';
-import styles from './App.module.css';
-import { DeviceFrameset } from 'react-device-frameset';
-import 'react-device-frameset/styles/marvel-devices.min.css';
-import { useOnClickOutside } from 'usehooks-ts';
-import { supportedAppStyles, supportedSpendContexts } from './constants';
-import { CancelIcon, Logo, Pencil } from './assets';
-import { Select, DollarInput } from './components';
-import { ColorPicker, Copy } from './containers';
-import { capitalizeFirst } from './util';
-
+import { useMediaQuery } from "react-responsive";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import _ from "lodash";
+import styles from "./App.module.css";
+import { DeviceFrameset } from "react-device-frameset";
+import "react-device-frameset/styles/marvel-devices.min.css";
+import { useOnClickOutside } from "usehooks-ts";
+import { supportedAppStyles, supportedSpendContexts } from "./constants";
+import { CancelIcon, Logo, Pencil } from "./assets";
+import { Select, DollarInput } from "./components";
+import { ColorPicker, Copy } from "./containers";
+import { capitalizeFirst } from "./util";
 
 function App() {
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1024px)' })
-  const [showMobileControls, setShowMobileControls] = useState(!isTabletOrMobile);
-  const [spendContext, setSpendContext] = useState('web');
-  const [xAppStyle, setXAppTheme] = useState('light');
-  const [primaryColor, setPrimaryColor] = useState('#670a78');
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+  const [showMobileControls, setShowMobileControls] = useState(
+    !isTabletOrMobile
+  );
+  const [spendContext, setSpendContext] = useState("web");
+  const [xAppStyle, setXAppTheme] = useState("light");
+  const [primaryColor, setPrimaryColor] = useState("#670a78");
   const [maxAmountInUSD, setMaxAmountInUSD] = useState<string>("100");
   const memoizedSpendContext = useMemo(() => spendContext, [spendContext]);
   const memoizedXAppStyle = useMemo(() => xAppStyle, [xAppStyle]);
   const memoizedPrimaryColor = useMemo(() => primaryColor, [primaryColor]);
+  const [chimoneyObject, setChimoneyObject] = useState<string>();
+  const chimoneyObjectRef = useRef<HTMLDivElement>(null);
   const memoizedMaxAmountInUSD = useMemo(
     () => maxAmountInUSD,
-    [maxAmountInUSD],
+    [maxAmountInUSD]
   );
   const chiSpendUrl = useMemo(
     () =>
-      `https://chispend.com/?cSContext=${memoizedSpendContext}${memoizedPrimaryColor
-        ? `&primaryColor=${memoizedPrimaryColor.substring(1)}`
-        : ''
-      }${memoizedXAppStyle ? `&xAppStyle=${memoizedXAppStyle}` : ''}${memoizedMaxAmountInUSD
-        ? `&maxAmountInUSD=${memoizedMaxAmountInUSD}`
-        : ''
+      `https://chispend.com/?cSContext=${memoizedSpendContext}${
+        memoizedPrimaryColor
+          ? `&primaryColor=${memoizedPrimaryColor.substring(1)}`
+          : ""
+      }${memoizedXAppStyle ? `&xAppStyle=${memoizedXAppStyle}` : ""}${
+        memoizedMaxAmountInUSD
+          ? `&maxAmountInUSD=${memoizedMaxAmountInUSD}`
+          : ""
       }`,
     [
       memoizedMaxAmountInUSD,
       memoizedXAppStyle,
       memoizedPrimaryColor,
       memoizedSpendContext,
-    ],
+    ]
   );
   const controlsRef = useRef(null);
 
-
   const handleChange = (
-    event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>,
+    event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
   ) => {
     switch (event.target.name) {
-      case 'spendContext':
+      case "spendContext":
         setSpendContext(event.target.value);
         break;
-      case 'xAppTheme':
+      case "xAppTheme":
         setXAppTheme(event.target.value);
         break;
-      case 'maxAmountInUSD':
+      case "maxAmountInUSD":
         setMaxAmountInUSD(event.target.value);
         break;
       default:
@@ -71,31 +75,48 @@ function App() {
     if (!controlsRef) return;
     const controls = controlsRef.current as unknown as HTMLDivElement;
     controls.style.cssText = `
-    visibility: ${!showMobileControls && isTabletOrMobile ? 'hidden' : 'visible'};
-    transform: ${!showMobileControls && isTabletOrMobile ? 'translateY(-100%)' : 'translateY(0%)'};
+    visibility: ${
+      !showMobileControls && isTabletOrMobile ? "hidden" : "visible"
+    };
+    transform: ${
+      !showMobileControls && isTabletOrMobile
+        ? "translateY(-100%)"
+        : "translateY(0%)"
+    };
     `;
   }, [showMobileControls, isTabletOrMobile]);
 
   const handleClickOutside = () => {
     setShowMobileControls(false);
-  }
+  };
 
   useEffect(() => {
-    const handleMessage = (e: MessageEvent<{
-      chimoneys?: any[];
-    }>) => {
-      if (spendContext !== 'web') return;
+    const handleMessage = (
+      e: MessageEvent<{
+        chimoneys?: any[];
+      }>
+    ) => {
+      if (spendContext !== "web" && spendContext !== "mobile") return;
       if (!e.data?.chimoneys) return;
-      alert(`Here is the chimoney object for your transaction:\n${JSON.stringify(e.data, null, 2)}`);
-    }
+      setChimoneyObject(
+        `Here is the chimoney object for your transaction:\n${JSON.stringify(
+          e.data,
+          null,
+          2
+        )}`
+      );
+      chimoneyObjectRef.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    };
     window.addEventListener("message", handleMessage, false);
     return () => {
       window.removeEventListener("message", handleMessage, false);
-    }
+    };
   }, [spendContext]);
 
-
-  useOnClickOutside(controlsRef, handleClickOutside)
+  useOnClickOutside(controlsRef, handleClickOutside);
 
   return (
     <div className={styles.container}>
@@ -113,10 +134,10 @@ function App() {
             <iframe
               src={chiSpendUrl}
               style={{
-                height: '100%',
-                width: '100%',
-                outline: 'none',
-                border: 'none',
+                height: "100%",
+                width: "100%",
+                outline: "none",
+                border: "none",
               }}
               title="ChiSpend Widget"
             />
@@ -126,24 +147,27 @@ function App() {
           <iframe
             src={chiSpendUrl}
             style={{
-              height: '100%',
-              width: '100%',
-              outline: 'none',
-              border: 'none',
+              height: "100%",
+              width: "100%",
+              outline: "none",
+              border: "none",
             }}
             title="ChiSpend Widget"
           />
         </div>
-        <div className={styles.customization_widget__container} ref={controlsRef}>
-
+        <div
+          className={styles.customization_widget__container}
+          ref={controlsRef}
+        >
           <div className={styles.header__mobile}>
-            <CancelIcon onClick={() => {
-              setShowMobileControls(false);
-            }} />
+            <CancelIcon
+              onClick={() => {
+                setShowMobileControls(false);
+              }}
+            />
             <Logo />
             <p>Easily embed the ChiSpend customisable market place </p>
           </div>
-
 
           <h4> Customisation widget</h4>
           <hr />
@@ -166,13 +190,17 @@ function App() {
               onChange={(color) => {
                 const setColor = _.debounce(
                   () => setPrimaryColor(color.hex),
-                  50,
+                  50
                 );
                 setColor();
               }}
             />
             <p> Change max amount user can spend in USD: </p>
-            <DollarInput name="maxAmountInUSD" value={memoizedMaxAmountInUSD} onChange={handleChange} />
+            <DollarInput
+              name="maxAmountInUSD"
+              value={memoizedMaxAmountInUSD}
+              onChange={handleChange}
+            />
             <p> Change Spend Context: </p>
             <span>Spend context is the preferred payment channel/method.</span>
             <Select
@@ -190,11 +218,15 @@ function App() {
             <Copy
               text={chiSpendUrl}
               onCopy={() => {
-                alert('Copied customized ChiSpend to clipboard!');
+                alert("Copied customized ChiSpend to clipboard!");
               }}
             />
           </div>
         </div>
+      </div>
+
+      <div ref={chimoneyObjectRef}>
+        {chimoneyObject && <pre>{chimoneyObject}</pre>}
       </div>
     </div>
   );
